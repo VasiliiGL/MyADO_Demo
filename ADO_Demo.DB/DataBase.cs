858516ffaf8c40766427865981d08e4aabdd5423
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-05-25 20:24:31 +0400

    ADD MySQLData

--- ADO_Demo.DB/DataBase.cs
+++ ADO_Demo.DB/DataBase.cs
@@ -1,8 +1,26 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
+//using System.Data.Common;
 
 namespace ADO_Demo.DB
 {
     public class DataBase
     {
+        //private DbConnection _db;
+
+        //public DataBase(DbConnection db)
+        //{
+        //    _db = db;
+        //}
+
+        private MySqlConnection _db;
+        private const string Str = "Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;";
+
+        public DataBase()
+        {
+            _db = new MySqlConnection (Str);
+        }
+
+
     }
 }
