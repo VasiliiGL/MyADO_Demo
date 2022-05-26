6e2b4330d2bb2bb6438e9114c1a499f79c2f9970
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-05-26 21:05:59 +0400

    Update

--- ADO_Demo.DB/DataBase.cs
+++ ADO_Demo.DB/DataBase.cs
@@ -1,5 +1,7 @@
-﻿using MySql.Data.MySqlClient;
+﻿using ADO_Demo.DB.Config;
+using MySql.Data.MySqlClient;
 using System;
+using System.IO;
 //using System.Data.Common;
 
 namespace ADO_Demo.DB
@@ -14,13 +16,16 @@ namespace ADO_Demo.DB
         //}
 
        
-        private const string Str = "Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;";
+       // private const string Str = "Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;";
         protected MySqlConnection _db;
         protected MySqlCommand _command;
 
         protected DataBase()
         {
-            _db = new MySqlConnection (Str);
+            
+            using var file = new StreamReader("config_db.json");
+            var condig =ConfigDb.ImportFromJson(file.ReadToEnd());
+            _db = new MySqlConnection (condig.ToString());
             _command = new MySqlCommand { Connection =_db};
             //_command.Connection = _db;
         }
