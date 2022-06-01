a6e72bb55b552c1ee7bf374742e183d418f5e5c4
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-06-01 21:17:41 +0400

    ADD User

---
+++ ADO_Demo.DB/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADO_Demo.DB.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //public IEnumerable<string> Emails { get; set; }
+}
+}
