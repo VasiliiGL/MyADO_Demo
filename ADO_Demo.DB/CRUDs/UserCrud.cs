fa0941a952f350921b5acd8b0f0173e302287a82
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-06-01 21:18:22 +0400

    ADD UserCrud

---
+++ ADO_Demo.DB/CRUDs/UserCrud.cs
@@ -0,0 +1,25 @@
+﻿using ADO_Demo.DB.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADO_Demo.DB.CRUDs
+{
+    public class UserCrud : DataBase
+    {
+        public UserCrud() : base() 
+        {
+            Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            _db.Open();
+            var sql = "SELECT * FROM tab_users";
+            var result = _db.Query<User>(sql);
+            _db.Close();
+            return result;
+        }
+    }
+}
