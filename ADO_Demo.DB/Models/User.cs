98164e68b5672255d0b982522b5301e9eaf3b7d7
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-06-02 20:40:13 +0400

    Update User

--- ADO_Demo.DB/Models/User.cs
+++ ADO_Demo.DB/Models/User.cs
@@ -4,11 +4,58 @@ using System.Text;
 
 namespace ADO_Demo.DB.Models
 {
-    public class User
+    //public class User
+    //{
+    //    public int id { get; set; }
+    //    public string FirstName { get; set; }
+    //    public string LastName { get; set; }
+    //    public IEnumerable<string> Emails { get; set; }
+    //}
+
+    public class User//: IEquatable<User>
     {
         public int id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        //public IEnumerable<string> Emails { get; set; }
-}
+        public IEnumerable<string> Emails { get; set; }
+
+        //bool IEquatable<User>.Equals(User other)
+        //{
+        //    if (ReferenceEquals(null, other))
+        //    {
+        //        return false;
+        //    }
+        //    if (ReferenceEquals(this, other))
+        //    {
+        //        return true;
+        //    }
+
+        //    return id == other.id && FirstName == other.FirstName && LastName == other.LastName && Equals(Emails, other.Emails);
+
+        //}
+        //public override bool Equals(object obj)
+        //{
+        //    if (ReferenceEquals(null, obj))
+        //    {
+        //        return false;
+        //    }
+        //    if (ReferenceEquals(this, obj))
+        //    {
+        //        return true;
+        //    }
+        //    if (obj.GetType()!= this.GetType())
+        //    {
+        //        return false;
+        //    }
+
+        //    return Equals((User)obj);
+        //}
+
+        //public override int GetHashCode()
+        //{
+        //    return HashCode.Combine(id, FirstName, LastName, Emails);
+        //}
+
+    }
+
 }
