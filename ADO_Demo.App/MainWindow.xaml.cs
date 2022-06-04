67920705500a6f7e224d79f7255abec1affe01b1
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-06-04 14:45:40 +0400

    Update

--- ADO_Demo.App/MainWindow.xaml.cs
+++ ADO_Demo.App/MainWindow.xaml.cs
@@ -24,12 +24,41 @@ namespace ADO_Demo.App
     public partial class MainWindow : Window
     {
         public ObservableCollection<Role> Roles { get; set; }
+        public ObservableCollection<User> Users { get; set; }
+        public ObservableCollection<Account> Accounts {get; set;}
         public MainWindow()
         {
 
             var roles = new RoleCrud();
             Roles = new ObservableCollection<Role>(roles.GetAllRoles());
+            var users = new UserCrud();
+            Users = new ObservableCollection<User>(users.GetAllUsers());
+            var accounts = new AccountCrud();
+            Accounts = new ObservableCollection<Account>(accounts.GetAllAcounts());
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool initialization = false;
+            foreach (var account in Accounts)
+            {
+                if ((account.Login== InputLogin.Text) & (account.Password == InputPassword.Text))
+                {
+                    MessageBox.Show("Successfully. Authorization passed!");
+                    initialization = true;
+                    var accountWindow = new AccountWindow(account);
+                    accountWindow.Show();
+                    this.Close();
+                }   
+            }
+            if (initialization == false)
+            {
+                MessageBox.Show("Authorization failed!");
+            }
+               
+
+
         }
     }
 }
