67ffc188b59369868e0b99720db70253928d1839
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-06-04 14:41:31 +0400

    ADD Models Account

---
+++ ADO_Demo.DB/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADO_Demo.DB.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
