e6cdc031a1726bd9abfe63f5ffb86f122ba0008a
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-06-04 14:42:17 +0400

    ADD AccountCrudTest

---
+++ ADO_Demo.DB.Test/AccountCrudTest.cs
@@ -0,0 +1,48 @@
+﻿using ADO_Demo.DB.CRUDs;
+using ADO_Demo.DB.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ADO_Demo.DB.Test
+{
+    public class AccountCrudTest
+    {
+        [Fact]
+        public void GetAllAccount_Test()
+        {
+            var expectedAccount = new List<Account>
+            {
+                new Account
+                {
+                    Id = 1,
+                    Login = "_guest",
+                    Password = "123",
+                    RoleId = 1,
+                    IsActive = true
+                },
+                new Account
+                {
+                    Id = 2,
+                    Login = "anna",
+                    Password = "12345",
+                    RoleId = 2,
+                    IsActive = true
+                },
+                 new Account
+                {
+                    Id = 3,
+                    Login = "ad",
+                    Password = "123",
+                    RoleId = 3,
+                    IsActive = true
+                }
+            };
+
+            var accountCrud = new AccountCrud();
+            var actualAccount = accountCrud.GetAllAcounts();
+            Assert.Equal(expectedAccount, actualAccount);
+        }
+    }
+}
